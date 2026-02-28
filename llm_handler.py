a6d8b832733784daa1df15e3ea56ef7b61970c54
Roshinishasha <roshinishasha19@gmail.com>
2026-02-28 19:39:15 +0530

    Update llm_handler.py

--- llm_handler.py
+++ llm_handler.py
@@ -1,1 +1,15 @@
+import requests
 
+OLLAMA_URL = "http://localhost:11434/api/chat"
+
+def ask_phi(messages):
+    response = requests.post(
+        OLLAMA_URL,
+        json={
+            "model": "phi3:mini",
+            "messages": messages,
+            "stream": False
+        }
+    )
+
+    return response.json()["message"]["content"]
